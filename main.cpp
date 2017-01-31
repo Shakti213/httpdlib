3c40dab199223537d6638251821baeee0d65a1e8
Andreas Wass <andreas.wass@dalelven.com>
2017-01-31 08:03:28 +0100

    Fixed formatting

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <QCoreApplication>
-#include <string>
-#include "webserver.h"
 #include "httpdlib/request.h"
+#include "webserver.h"
+#include <QCoreApplication>
+#include <iostream>
+#include <string>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
 
     WebServer web_server;
